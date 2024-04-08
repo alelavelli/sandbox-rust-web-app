85af3089444f402122653a99d6a47f79d59954a1
Alessandro Lavelli <alessandro.lavelli@mlcube.com>
2024-04-08 07:50:39 +0200

    internal error handling

--- src/router.rs
+++ src/router.rs
@@ -1,9 +1,17 @@
-use axum::{http::StatusCode, routing::post, Json, Router};
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
 
 /// Defines the router user that is imported by the main application Router
 pub fn get_user_router() -> Router {
-    Router::new().route("/", post(create_user))
+    Router::new()
+        .route("/", post(create_user))
+        .route("/", get(handler))
+        .route("/user", get(get_user))
 }
 
 async fn create_user(
@@ -33,4 +41,57 @@ struct CreateUser {
 struct User {
     id: u64,
     username: String,
+}
+
+#[derive(Deserialize)]
+struct GetUser {
+    username: String,
+}
+
+async fn handler() -> Result<(), AppError> {
+    try_thing()?;
+    Ok(())
+}
+
+fn try_thing() -> Result<(), anyhow::Error> {
+    anyhow::bail!("it failed!")
+}
+
+async fn get_user(
+    Json(payload): Json<GetUser>
+) -> Result<(StatusCode, Json<User>), AppError>
+{
+    let user = make_db_query(payload.username)?;
+    Ok((StatusCode::OK, Json(user)))
+}
+
+// Mock function that returns an error
+fn make_db_query(_username: String) -> Result<User, anyhow::Error> {
+    anyhow::bail!("internal connection failed")
+}
+
+
+// Make our own error that wraps `anyhow::Error`.
+struct AppError(anyhow::Error);
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
 }
